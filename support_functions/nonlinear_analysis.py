12fee48846e7e920bb5df2860ebe1071ef26d9a9
Jill Emmerzaal <jillemmerzaal@gmail.com>
2024-06-26 13:33:24 -0400

    cleaning packages

--- support_functions/nonlinear_analysis.py
+++
@@ -1,9 +0,0 @@
-from support_functions.grab import grab
-from support_functions.engine import engine
-
-
-def nld_analysis(fld):
-    fl = engine(path=fld, extension=".zoo")
-
-    for f in fld:
-        data = grab(f)
